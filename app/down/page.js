a1ef0a69c8555293bd29b133541c51c2c72d3afb
lijan09 <lijanaayasta@gmail.com>
2023-09-14 00:00:58 +0545

    Maths Paper Leaked

--- app/down/page.js
+++ app/down/page.js
@@ -6,7 +6,7 @@ const page = () => {
 
       <p>
           <button className="rounded bg-[#006a4e] text-gray-100 cursor-pointer hover:bg-[#2a8000]">
-            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noreferrer">CLICK HERE FOR EM NOTES</a>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noreferrer">CLICK HERE FOR LEAKED MATHS PAPER!!!</a>
           </button>
       </p>
     </div>
